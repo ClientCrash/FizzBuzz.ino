0e5963dd4c1a1092ee21670b482e957a5bf70bdc
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2021-10-14 22:11:05 +0200

    added logic

--- arduino-fizz-buzz.ino
+++ arduino-fizz-buzz.ino
@@ -1,9 +1,55 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);   
+  run();             
 
+}
+
+void run(){
+  int x = 0;
+  while(x<100){
+    
+    x+=1;
+      
+     if (x % 3 == 0){
+            Serial.println("Fizz");
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(100);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(100);
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(100);
+            digitalWrite(LED_BUILTIN, LOW);
+     }
+     if (x % 5 == 0){
+            Serial.println("Buzz");
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(100);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(100);
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(100);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(100);
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(100);
+            digitalWrite(LED_BUILTIN, LOW);
+     }
+     if ((x % 3 != 0) && (x % 5 != 0)){
+            Serial.println(x);
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(1000);
+            digitalWrite(LED_BUILTIN, LOW);
+     }
+     
+     delay(2000);
+  }
+    
 }
